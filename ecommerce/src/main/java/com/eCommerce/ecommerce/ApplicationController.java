19bb5fda9d64f9ccd134cad871cfcba24f52db54
Thejesh1698 <thejesh1698@gmail.com>
2020-07-08 21:30:16 +0530

    basic_setup_ended

---
+++ ecommerce/src/main/java/com/eCommerce/ecommerce/ApplicationController.java
@@ -0,0 +1,15 @@
+package com.eCommerce.ecommerce;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ApplicationController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "home";
+    }
+
+}
